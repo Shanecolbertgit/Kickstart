92fb87ca39ce1c1eb28c91b90185393d4ae22da4
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-11-05 13:28:14 +0000

    update

--- ethereum/test/Campaign.test.js
+++ ethereum/test/Campaign.test.js
@@ -34,7 +34,11 @@ beforeEach(async () => {
 describe('Campaigns', () => {
     it('deploys a factory and a campaign', () => {
         assert.ok(factory.options.address);
-        assert.ok(camapaign.options.address);
+        assert.ok(campaign.options.address);
     });
 
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
 });
