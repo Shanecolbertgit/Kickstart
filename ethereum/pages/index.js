3adb0045703001bf9476c7cf9471261471d82d7f
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-11-08 09:13:10 +0000

    Update index.js

--- ethereum/pages/index.js
+++ ethereum/pages/index.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
-    async componentDidMount() {
+    static async getInitialPtops() {
         const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-        console.log(campaigns);
+        return { campaigns };
     }
 
     render() {
