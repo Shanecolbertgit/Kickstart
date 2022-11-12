17ea24873f0118f69a8c079f727e6e9c45f6e1bb
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-11-12 13:00:42 +0000

    update

--- ethereum/pages/campaigns/show.js
+++ ethereum/pages/campaigns/show.js
@@ -1,7 +1,19 @@
 import react, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../ethereum/campaign';
 
 class CampaignShow extends Component {
+
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
+
+        const summery = await CampaignShow.metiods.getSummery().call();
+
+        console.log(summery);
+
+
+        return {};
+    }
     render() {
         return (
             <Layout>
