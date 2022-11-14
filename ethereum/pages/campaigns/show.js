8143faa2ab8f0146c33b9371caf9a4c2b73e4616
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-11-14 13:05:26 +0000

    Update show.js

--- ethereum/pages/campaigns/show.js
+++ ethereum/pages/campaigns/show.js
@@ -1,4 +1,5 @@
 import react, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 
@@ -22,15 +23,57 @@ class CampaignShow extends Component {
     }
 
     renderCards() {
-        const items = 
+        const {
+            balance,
+            manager,
+            minimumCountribution,
+            requestCountapprobersCount
+
+        } = this.props;
+        const items = [
+            {
+                header: manager,
+                meta: 'Address of manager',
+                description: 'The manager  created this campaign and can create requests to witraw money',
+                style: { overflowWrap: 'break-word' }
+
+
+            },
+            {
+
+                header: minimumCountribution,
+                meta : 'Minimum Contribution (wei)',
+                discription: 'You must contibute at least this much wei to become an approver'
+
+            },
+            {
+
+                header: requestCount,
+                meta: 'Number of requests',
+                description: 'A request tries to withdraw money from the contract. Request must be approved by approvers'
+            },
+            {
+                header: approversCount,
+                meta: 'Numbers of Approvers',
+                description: 'Number of people who have already donated to this campaign'
+            },
+
+            {
+                
+
+            }
+        ];
+
+        return <Card.Group items={items} />;
     }
     render() {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
+                {this.renderCards()}
             </Layout>
-        );
-    }
+        )
+    };
 }
 
 export default CampaignShow;
