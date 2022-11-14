36edf58a3b72fecb30388962d30df9cef8b9e601
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-11-14 18:09:36 +0000

    index.js file

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ const routes = require('next-routes')();
 routes
     .add('/campagins/new', '/campaigns/new')
 
-    .add('/campaigns/:address', '/camapigns/show');
+    .add('/campaigns/:address', '/camapigns/show')
+    .add('/campaigns/:address/requests', '/campaigns.requests/index');
 
 module.exports = routes;
 
