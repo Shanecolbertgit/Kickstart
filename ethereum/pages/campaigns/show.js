32636f4f07a9c1aa763306048716f18ff111a9f4
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-11-14 13:58:49 +0000

    Contribute form

--- ethereum/pages/campaigns/show.js
+++ ethereum/pages/campaigns/show.js
@@ -1,7 +1,9 @@
 import react, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+import web3 from '../../ethereum/web3';
+import ContributeForm from '../../components/ContributeForm';
 
 class CampaignShow extends Component {
 
@@ -34,7 +36,7 @@ class CampaignShow extends Component {
             {
                 header: manager,
                 meta: 'Address of manager',
-                description: 'The manager  created this campaign and can create requests to witraw money',
+                description: 'The manager created this campaign and can create requests to witraw money',
                 style: { overflowWrap: 'break-word' }
 
 
@@ -42,7 +44,7 @@ class CampaignShow extends Component {
             {
 
                 header: minimumCountribution,
-                meta : 'Minimum Contribution (wei)',
+                meta: 'Minimum Contribution (wei)',
                 discription: 'You must contibute at least this much wei to become an approver'
 
             },
@@ -59,8 +61,9 @@ class CampaignShow extends Component {
             },
 
             {
-                
-
+                header: web3.utils.fromWei(balance, 'ether'),
+                meta: ' Campaign Balance (Eether)',
+                description: 'This balance is how much money this campaign has left to spend'
             }
         ];
 
@@ -70,7 +73,18 @@ class CampaignShow extends Component {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
-                {this.renderCards()}
+                <Grid>
+                    <Grid.Column width={10}>
+                        {this.renderCards()}
+                    </Grid.Column>
+
+                    <Grid.Column width={6}>
+                        <ContributeForm />
+                    </Grid.Column>
+
+                </Grid>
+
+
             </Layout>
         )
     };
